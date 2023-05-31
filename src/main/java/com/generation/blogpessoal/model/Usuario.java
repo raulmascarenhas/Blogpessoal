a0ccef180ccbb0eea68778ce872d740829876025
Raul Mascarenhas <mascraul@hotmail.com>
2023-05-31 14:03:51 -0300

    atualizando JUNIT

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -42,7 +42,21 @@ public class Usuario {
 	@OneToMany(fetch = FetchType.LAZY,mappedBy = "usuario",cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+	
+	
 
+	public Usuario(Long id,String nome,String usuario, String senha, String foto) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+public Usuario() {
+	
+}
+	
 	public Long getId() {
 		return id;
 	}
